30a9632cb8476454572eb3ee9770b25561ed0284
Szyme <mail>
2025-06-21 11:29:49 +0200

    dodano nowe zmiany

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import generator_kodu
+from interfejs import MastermindGUI
+
 
 def mastermind():
     ukryty_kod = generator_kodu.KodGenerator().generuj_kod(4, unikalne=False)
+    app = MastermindGUI()
+    app.create_widgets()
+    app.mainloop()
 
 
 mastermind()
